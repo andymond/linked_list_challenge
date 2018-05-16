a259d48ae60122b4b19fb25993aca74ad75c7b11
Andrew Dymond <andy.dymond@gmail.com>
2018-05-16 09:28:44 -0600

    add append to linked_list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,16 @@
+require "./lib/node"
+
 class LinkedList
   attr_reader :head
 
   def initialize
     @head = nil
+    @next_node = nil
+  end
+
+  def append(node)
+    @head = Node.new(node) if head.nil?
+    node
   end
 
 end

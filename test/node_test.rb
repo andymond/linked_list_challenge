c7f66ec5892066f4ced2a5f3738458ff401ee1da
Andrew Dymond <andy.dymond@gmail.com>
2018-05-16 09:23:45 -0600

    Add node tests

---
+++ test/node_test.rb
@@ -0,0 +1,20 @@
+require "./lib/node"
+require "minitest/pride"
+require "minitest/autorun"
+require "pry"
+
+class NodeTest < Minitest::Test
+
+  def test_it_initializes_with_data
+    node = Node.new(5)
+
+    assert_equal 5, node.data
+  end
+
+  def test_it_initializes_with_nil_next_node
+    node = Node.new(5)
+
+    assert_nil node.next_node
+  end
+
+end
